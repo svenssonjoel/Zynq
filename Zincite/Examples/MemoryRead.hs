5bca10ffa4b47a2615c7692fa07c2bbc534c2fd2
Joel Svensson <svenssonjoel@yahoo.se>
2016-11-29 20:51:37 +0100

    fixed memoryread example for changes in API

--- Zincite/Examples/MemoryRead.hs
+++ Zincite/Examples/MemoryRead.hs
@@ -5,18 +5,18 @@ module MemoryRead where
 import Zincite.Syntax
 import Zincite.GenCpp
 
-addMem :: InterfaceIO -> Compute ()
+addMem :: Memory Global -> Compute ()
 addMem io =
   do
     mwrite io address1 $ (mread io address2 + mread io (address2 + 4) :: Expr Int)
 
-    where address1 = 10000 :: Expr Int
-          address2 = 20000 :: Expr Int
+    where address1 = 10000 :: Expr Address
+          address2 = 20000 :: Expr Address
     
 
 
 
 addMemAST =
-  let io = (InterfaceIO "io")
+  let io = M (InterfaceIO "io")
   in snd $ runCompute $ addMem io  
 
