72f9a278febe97d503007541fb2ad6213758eb35
Joel Svensson <svenssonjoel@yahoo.se>
2016-11-30 11:42:02 +0100

    updated Zincite.Lava module.

--- Zincite/Zincite/Lava.hs
+++ Zincite/Zincite/Lava.hs
@@ -7,7 +7,7 @@ module Zincite.Lava where
 
 import Zincite.Syntax
 
-
+import Prelude hiding (min,max) 
 
 
 ------------------------------------------------------------
@@ -17,7 +17,9 @@ listFromMem mem addr n =
   [ mread mem (fromIntegral ix)|  ix <- [0..(n-1)]] 
 
 
-
+------------------------------------------------------------
+--
+(>->) = flip (.) 
 
 ------------------------------------------------------------
 --
@@ -42,3 +44,69 @@ parl f g xs   = f (take nhalf xs) ++ g (drop nhalf xs)
 
 two :: ([a] -> [b]) -> [a] -> [b]
 two f = parl f f 
+
+
+------------------------------------------------------------
+--
+
+unpair :: [(a,a)] -> [a]
+unpair [] = []
+unpair ((x,y):xs) = x : y : unpair xs 
+
+pair :: [a] -> [(a,a)]
+pair [] = []
+pair [a] = error "pair: input list is too short" -- really check for even length
+pair (x:y:xs) = (x,y) : pair xs
+
+
+halve :: [a] -> ([a],[a])
+halve [] = ([],[])
+halve xs = (pre,post) 
+  where n = length xs
+        nhalf = n `div` 2
+        pre = take nhalf xs
+        post = drop nhalf xs
+
+------------------------------------------------------------
+--
+
+riffle :: [a] -> [a]
+riffle [] = []
+riffle [x] = [x] 
+riffle xs = unpair $ zip pre post  
+  where
+    (pre,post) = halve xs 
+
+unriffle [] = []
+unriffle [x] = [x] 
+unriffle xs = let (pre,post) = unzip (pair xs)
+              in pre ++ post 
+
+
+------------------------------------------------------------
+--
+
+ilv :: ([a] -> [b]) -> [a] -> [b]
+ilv f = riffle . two f . unriffle 
+
+
+------------------------------------------------------------
+-- butterfly
+bfly :: (Eq a, Num a) => a -> ([a1] -> [a1]) -> [a1] -> [a1]
+bfly 1 f = f
+bfly n f = ilv (bfly (n-1) f) >-> evens f  
+
+
+------------------------------------------------------------
+-- two sorter
+twosort :: Emb a =>  [a] -> [a]
+twosort xs = unpair (map minmax (pair xs)) -- check lengths and signal error
+  where minmax p = (min p, max p)  
+
+
+------------------------------------------------------------
+-- Batchers
+sortB :: (Eq a, Num a) => a -> ([b] -> [b]) -> [b] -> [b]
+sortB 0 cmp = id
+sortB n cmp = parl (sortB k cmp) (sortB k cmp >-> reverse) >-> bfly n cmp  
+  where k = n - 1
