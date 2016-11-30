063cc9cb5f6444014d453eeee6b2e90dd2267720
Joel Svensson <svenssonjoel@yahoo.se>
2016-11-30 20:21:13 +0100

    preparations for sharing detection on exp level

--- Zincite/Zincite/GenCpp.hs
+++ Zincite/Zincite/GenCpp.hs
@@ -11,19 +11,3 @@ import Zincite.Syntax
 -- TODO: To generate memory reads we need type information 
 
 
-
-pexp :: Exp -> String
-pexp (Constant (IntVal i)) = show i
-pexp (Constant (BoolVal True)) = "1";
-pexp (Constant (BoolVal False)) = "0";
-pexp (Constant (FloatVal f)) = show f
-pexp (BinOp op e1 e2) = "(" ++ pexp e1 ++ binop op ++ pexp e2 ++ ")"
-pexp (UnOp  op e1)    = "(" ++ unop op ++ pexp e1 ++ ")"  
-
-
-binop Add = " + "
-binop Sub = " - "
-binop Mul = " * "
-binop Div = " / "
-
-unop neg = " - " 
--- Zincite/Zincite/Syntax.hs
+++ Zincite/Zincite/Syntax.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Zincite.Syntax where 
@@ -47,15 +48,36 @@ data Value = IntVal Int
                       
 -- expression language
 -- TODO: Support for Tuples 
-data Exp =
+data ExpNode s =
   Constant Value
   | Variable Name 
-  | BinOp Op2 Exp Exp
-  | UnOp  Op1 Exp 
-  | MRead MemoryInternal Type Exp -- Read from Interface at address
-  | Tuple [(Exp,Type)] -- Internal representation of tuples
+  | BinOp Op2 s s
+  | UnOp  Op1 s 
+  | MRead MemoryInternal Type s -- Read from Interface at address
+  | Tuple [(s,Type)] -- Internal representation of tuples
  -- | LRead LocalMem Type Exp    -- Read from local memory at address
-    deriving Show 
+    deriving Show
+
+constantE :: Value -> Exp
+constantE v = Exp $ Constant v
+
+variableE :: Name -> Exp
+variableE nom = Exp $ Variable nom
+
+binOpE :: Op2 -> Exp -> Exp -> Exp
+binOpE op e1 e2 = Exp $ BinOp op e1 e2
+
+unOpE :: Op1 -> Exp -> Exp
+unOpE op e1 = Exp $ UnOp op e1
+
+mreadE :: MemoryInternal -> Type -> Exp -> Exp
+mreadE m t e = Exp $ MRead m t e
+
+tupleE :: [(Exp,Type)] -> Exp
+tupleE ls = Exp $ Tuple ls 
+
+data Exp = Exp {expNode :: ExpNode Exp}
+  deriving Show
 
 data Op2 =
   Add | Sub | Mul | Div | Mod
@@ -66,46 +88,53 @@ data Op1 =
   | Min | Max -- Expects argument to be a tuple 
   deriving Show 
 
-
 newtype Expr a = E {unE :: Exp}
+
+unExpr (E (Exp e)) = e 
+
+expr :: ExpNode Exp -> Expr a
+expr e = E $ Exp e 
 
 ------------------------------------------------------------
 -- Arithmetic and operations
 
 
 instance Num (Expr Int) where
-  (+) a b = E $ BinOp Add (unE a) (unE b)
-  (-) a b = E $ BinOp Sub (unE a) (unE b)
-  (*) a b = E $ BinOp Mul (unE a) (unE b)
+  (+) a b = fromExp $ binOpE Add (unE a) (unE b)
+  (-) a b = fromExp $ binOpE Sub (unE a) (unE b)
+  (*) a b = fromExp $ binOpE Mul (unE a) (unE b)
   abs = undefined
   signum = undefined
-  fromInteger i = E $ Constant $ IntVal $ fromInteger i
+  fromInteger i = fromExp $ constantE $ IntVal $ fromInteger i
 
 instance Num (Expr Word) where
-  (+) a b = E $ BinOp Add (unE a) (unE b)
-  (-) a b = E $ BinOp Sub (unE a) (unE b)
-  (*) a b = E $ BinOp Mul (unE a) (unE b)
+  (+) a b = fromExp $ binOpE Add (unE a) (unE b)
+  (-) a b = fromExp $ binOpE Sub (unE a) (unE b)
+  (*) a b = fromExp $ binOpE Mul (unE a) (unE b)
   abs = undefined
   signum = undefined
-  fromInteger i = E $ Constant $ UIntVal $ fromInteger i
+  fromInteger i = fromExp $ constantE $ UIntVal $ fromInteger i
 
 -- TODO: Correct constraints on "a" for these functions 
 mod :: Emb a => a -> a -> a 
-mod a b = fromExp $ BinOp Mod (toExp a) (toExp b) 
+mod a b = fromExp $ binOpE Mod (toExp a) (toExp b) 
 
 div :: Emb a => a -> a -> a 
-div a b = fromExp $ BinOp Div (toExp a) (toExp b) 
+div a b = fromExp $ binOpE Div (toExp a) (toExp b) 
 
 min :: Emb a =>  (a, a) -> a
-min tup = fromExp $ UnOp Min (toExp tup)
+min tup = fromExp $ unOpE Min (toExp tup)
 
 max :: Emb a => (a, a) -> a 
-max tup = fromExp  $ UnOp Max (toExp tup) 
+max tup = fromExp  $ unOpE Max (toExp tup) 
 
 ------------------------------------------------------------
--- Booleans and bool ops 
-true = E $ Constant $ BoolVal True 
-false = E $ Constant $ BoolVal False
+-- Booleans and bool ops
+
+true :: Expr Bool 
+true = fromExp $ constantE $ BoolVal True
+false :: Expr Bool 
+false = fromExp $ constantE $ BoolVal False
 
 
 type Target = Name
@@ -163,14 +192,14 @@ class MemoryIO a where
 
 instance MemoryIO Int where
   mread interface addr =
-    E $ MRead (unM interface) TInt (unE addr) 
+    fromExp $ mreadE (unM interface) TInt (unE addr) 
     
   mwrite interface addr value =
     tell $ MWrite (unM interface) TInt (unE addr) (unE value) 
 
 instance MemoryIO Float where
   mread interface addr =
-    E $ MRead (unM interface) TFloat (unE addr) 
+    fromExp $ mreadE (unM interface) TFloat (unE addr) 
     
   mwrite interface addr value =
     tell $ MWrite (unM interface) TFloat (unE addr) (unE value) 
@@ -193,6 +222,11 @@ instance Emb (Expr Word) where
   toExp = unE
   fromExp = E 
 
+instance Emb (Expr Float) where
+  typeOf _ = TFloat
+  toExp = unE
+  fromExp = E 
+
 
 instance Emb (Expr Bool) where
   typeOf _ = TBool
@@ -201,13 +235,13 @@ instance Emb (Expr Bool) where
 
 instance (Emb a, Emb b) => Emb (a,b) where
   typeOf (a,b) = TTuple [typeOf a, typeOf b]
-  toExp  (a,b) = Tuple [(toExp a, typeOf a), (toExp b, typeOf b)]
-  fromExp (Tuple [(e1,t1),(e2,t2)]) = (fromExp e1, fromExp e2) 
+  toExp  (a,b) = tupleE [(toExp a, typeOf a), (toExp b, typeOf b)]
+  fromExp (Exp (Tuple [(e1,t1),(e2,t2)])) = (fromExp e1, fromExp e2) 
 
 instance (Emb a, Emb b, Emb c) => Emb (a,b,c) where
   typeOf (a,b,c) = TTuple [typeOf a, typeOf b,typeOf c]
-  toExp  (a,b,c) = Tuple [(toExp a, typeOf a), (toExp b, typeOf b), (toExp c, typeOf c)]
-  fromExp (Tuple [(e1,t1),(e2,t2), (e3,t3)]) = (fromExp e1, fromExp e2, fromExp e3) 
+  toExp  (a,b,c) = tupleE [(toExp a, typeOf a), (toExp b, typeOf b), (toExp c, typeOf c)]
+  fromExp (Exp (Tuple [(e1,t1),(e2,t2), (e3,t3)])) = (fromExp e1, fromExp e2, fromExp e3) 
   
 
 
@@ -217,23 +251,23 @@ declare :: forall a . Emb a => Compute (a)  -- SCOPED TYPE VARIABLE
 declare =
   do nom <- freshName "v"
      tell $ Declare nom (typeOf (undefined :: a)) -- SCOPED TYPE VARIABLE enables this
-     return $ fromExp (Variable nom) 
+     return $ fromExp (variableE nom) 
 
 infixl 1 =: 
 (=:) :: forall a . Emb a => a -> a -> Compute ()
 (=:) left right =
   case (toExp left,toExp right) of
-    (Variable nom, anything) -> tell $ Assign nom (typeOf right) anything 
+    (Exp (Variable nom), anything) -> tell $ Assign nom (typeOf right) anything 
     (_,_) -> error "assign to non-variable" 
     
 
 ------------------------------------------------------------
 -- Stream get and put 
-sget :: StreamIn a -> Compute (Expr a)
+sget :: Emb (Expr a) => StreamIn a -> Compute (Expr a)
 sget (SIn si@(StreamInternal _ typ)) =
   do
     nom <- freshName "s"
-    let var = E $ Variable nom
+    let var = fromExp $ variableE nom
     
     tell $ SGet nom si typ
     return var
@@ -253,13 +287,13 @@ bram n =
 
 ------------------------------------------------------------
 -- while
-while :: (Emb (a)) => (a -> Expr Bool) -> (a -> Compute ()) -> a -> Compute ()
+while :: (Emb (Expr a)) => (Expr a -> Expr Bool) -> (Expr a -> Compute ()) -> Expr a -> Compute ()
 while cond body init =
   do
     nom <- freshName "tmp" 
-    let var = Variable $ nom
-        bodyComp = body (fromExp var)
-        condExpr = cond (fromExp var)
+    let var  = fromExp $ variableE $ nom
+        bodyComp = body var
+        condExpr = cond var
     bodyCode <- runComputeLocal bodyComp
     tell $ Assign nom (typeOf init) (toExp init)      -- HACK 
     tell $ While nom bodyCode (unE condExpr)  
