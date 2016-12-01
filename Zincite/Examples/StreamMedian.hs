c49eddf3be4bc65472c651f88cb7667c3f8d3290
Joel Svensson <svenssonjoel@yahoo.se>
2016-12-01 14:47:40 +0100

    update

--- Zincite/Examples/StreamMedian.hs
+++ Zincite/Examples/StreamMedian.hs
@@ -27,7 +27,7 @@ streamMedian in1 out =
           mwrite lmem i a
 
           -- a better abstraction than pull arrays when dealing with statically known
-          -- amounts of data (that we want to do "unrolled" computations on, is a normal Haskell List
+          -- amounts of data (that we want to do "unrolled" computations on) is a normal Haskell List
           let sorted = sortB 3 twosort $ listFromMem lmem 0 8
               result = (sorted !! 3 + sorted !! 4) `div` 2 -- or go float ? 
           
