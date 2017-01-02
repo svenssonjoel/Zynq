23ab3bc4b9d39a89326efeffde9944cd64df564f
Joel Svensson <bo.joel.svensson@gmail.com>
2017-01-02 17:15:02 +0100

    Monadic streamMap example

--- Zincite/Examples/StreamMap.hs
+++ Zincite/Examples/StreamMap.hs
@@ -12,11 +12,32 @@ smap f in1 out =
   do a <- sget in1
      sput out (f a)
    
+-- This is a stream map kernel generator. 
+smapM :: (Emb b, Emb a) => (a -> Compute b) -> StreamIn a -> StreamOut b -> Compute ()
+smapM f in1 out =
+  forever $ 
+  do a <- sget in1
+     b <- f a
+     sput out b
 
+   
+   
 smapAST =
   let in1 = (SIn (StreamInternal "s1" TInt))
       out = (SOut (StreamInternal "s3" TInt))
   in snd $ runCompute $ smap f in1 out
   where
-    f :: Expr Int -> Expr Int 
+    f :: ZInt -> ZInt 
     f a = a + 1
+
+    
+    
+    
+smapMAST =
+  let in1 = (SIn (StreamInternal "s1" TInt))
+      out = (SOut (StreamInternal "s3" TInt))
+  in snd $ runCompute $ smapM f in1 out
+  where
+    f :: ZInt -> Compute ZInt
+    f a = return $ a + 1    
+    
